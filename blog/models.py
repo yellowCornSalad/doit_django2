2d8a5a0d4767bd3e944c27d6534de4bb180a2313
junho <zk836566@gmail.com>
2022-03-22 16:13:30 +0900

    Post 모델 생성 및 자동 시간 설정

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #author: 추후작성
+# Create your models here.
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
