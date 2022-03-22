27a8f2eee3469551a5eba7237ce67e2d079a2034
junho <zk836566@gmail.com>
2022-03-22 17:26:24 +0900

    블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,24 @@ from .models import Post
 
 
 def index(request):
-    posts = Post.objects.all().order_by('-pk')
+    posts = Post.objects.all().order_by('-pk')  # order_by('-pk') => 최신순 정렬
 
     return render(
         request,
         'blog/index.html',
         {'posts': posts, }
+    )
+
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
+        }
     )
 
 # Create your views here.
