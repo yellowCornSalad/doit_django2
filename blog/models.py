3354dba862a833c7015e0cd2ed7c2c92574d0fba
junho <zk836566@gmail.com>
2022-03-22 17:01:34 +0900

    블로그 포스트 목록 페이지 제작

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     #author: 추후작성
