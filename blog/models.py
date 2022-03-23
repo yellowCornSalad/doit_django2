e50f00403909a709ab279f64a9d72419ed185514
junho <zk836566@gmail.com>
2022-03-23 13:07:29 +0900

    author 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 
@@ -17,8 +18,10 @@ class Post(models.Model):
     #author: 추후작성
 # Create your models here.
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title}: : {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
