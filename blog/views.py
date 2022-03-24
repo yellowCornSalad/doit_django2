d00c91aa1914d825bcd9b439d8a970ed7e2381bd
junho <zk836566@gmail.com>
2022-03-24 18:05:05 +0900

    포스트를 수정할 때 텍스트로 태그 지정하기

--- blog/views.py
+++ blog/views.py
@@ -73,7 +73,7 @@ def tag_page(request, slug):
 class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content',
-              'head_image', 'file_upload', 'category', 'tags']  # 끝부분에 ,'tags': 태그생성자
+              'head_image', 'file_upload', 'category']  # 끝부분에 ,'tags': 태그생성자
 
     def form_valid(self, form):
         current_user = self.request.user  # self.request.user: 웹사이트의 방문자
@@ -92,7 +92,7 @@ class PostCreate(LoginRequiredMixin, CreateView):
                 tags_list = tags_str.split(';')
 
                 for t in tags_list:
-                    t = t.strip()  # 4
+                    t = t.strip()  # 4 공백제거
                     tag, is_tag_created = Tag.objects.get_or_create(
                         name=t)  # 5
                     if is_tag_created:  # 6
@@ -110,15 +110,47 @@ class PostCreate(LoginRequiredMixin, CreateView):
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'hook_text', 'content',
-              'head_image', 'field_upload', 'category', 'tags']
+              'head_image', 'file_upload', 'category', 'tags']
+
+    template_name = 'blog/post_update_form.html'
+
+    # PostUpdate에서 값을 넘기고 get_context_data에서 넘긴 값을 받음
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists():
+            tags_str_list = list()
+            for t in self.object.tags.all():
+                tags_str_list.append(t.name)
+            context['tags_str_default'] = '; '.join(tags_str_list)
+
+        return context
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
-            return super(PostUpdate, self).dispatch(request, *args, *kwargs)
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
+
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()  # 수정하기 전 원래 있던 값을 지워버림
+
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:
+            tags_str = tags_str.strip()
+            tags_str = tags_str.replace(',', ';')
+            tags_list = tags_str.split(';')
+
+            for t in tags_list:
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+        return response
     # cbv로 뷰를 만들 때 template_name을 지정해 원하는 html파일을 템플릿 파일로 설정
-    template_name = 'blog/post_update_form.html'
+
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')  # order_by('-pk') => 최신순 정렬
