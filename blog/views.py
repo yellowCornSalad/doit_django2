937116e26d6b1a019f0dab4e0724d93db0558766
junho <zk836566@gmail.com>
2022-03-24 16:42:09 +0900

    포스트를 작성할 때 텍스트로 태그 지정하기

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from urllib import request
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Category, Tag
+from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
 # ListView를 상속받아 만든 PostList 클래스
 
@@ -71,15 +73,52 @@ def tag_page(request, slug):
 class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content',
-              'head_image', 'file_upload', 'category']
+              'head_image', 'file_upload', 'category', 'tags']  # 끝부분에 ,'tags': 태그생성자
 
     def form_valid(self, form):
-        current_user = self.request.user
+        current_user = self.request.user  # self.request.user: 웹사이트의 방문자
+        # is_quthenticated: 방문자의 로그인상태
+        # 현재 접속유저가 staff권한이거나 superuser의 권한일 때
         if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
-            return super(PostCreate, self).form_valid(form)
+            # 1 CreateView의 form_vaild()함수의 결과 값을 response라는 변수에 임시로 달아둠
+            response = super(PostCreate, self).form_valid(form)
+
+            tags_str = self.request.POST.get('tags_str')  # 2
+            if tags_str:  # 3
+                tags_str = tags_str.strip()
+
+                tags_str = tags_str.replace(',', ';')
+                tags_list = tags_str.split(';')
+
+                for t in tags_list:
+                    t = t.strip()  # 4
+                    tag, is_tag_created = Tag.objects.get_or_create(
+                        name=t)  # 5
+                    if is_tag_created:  # 6
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)  # 7
+
+                return response
         else:
-            return redirect('/blog/')
+            return redirect('/blog/')  # 8
+
+# cbv 스타일로 postupdate 구현
+
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content',
+              'head_image', 'field_upload', 'category', 'tags']
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, *kwargs)
+        else:
+            raise PermissionDenied
+    # cbv로 뷰를 만들 때 template_name을 지정해 원하는 html파일을 템플릿 파일로 설정
+    template_name = 'blog/post_update_form.html'
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')  # order_by('-pk') => 최신순 정렬
