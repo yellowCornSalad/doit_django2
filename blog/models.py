604234de42a0f48420b2a2119d3810d44b5a7d85
junho <zk836566@gmail.com>
2022-03-25 14:49:28 +0900

    로그인 구현완료

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,17 @@ from markdownx.utils import markdown
 import os
 
 
+class Comment(models.Model):
+    # post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
